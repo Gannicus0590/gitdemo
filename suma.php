389d7cb49f0c95ff5c355563a2c69ff938f523a9
vagrant <vagrant@precise32.(none)>
2014-01-19 17:10:28 +0000

    primer commit agregamos archivo suma.php

---
+++ suma.php
@@ -0,0 +1,18 @@
+<?php
+namespace Hackspace;
+
+class Suma
+{
+	private $x;
+	private $y;
+
+	public function __construct($x,$y)
+	{
+		$this->x=$x;
+		$this->y=$y;
+	}
+	public function ejecutar()
+	{
+		return $this->x+$this->y;
+	}
+}
